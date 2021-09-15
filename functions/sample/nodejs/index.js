07a6185b42cfba59dd57a2ce9303adc4bd57c8ab
aneeshmraj <aneeshmraj@yahoo.com>
2021-09-15 08:06:42 +0000

    updated cloudant endpoints

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -1,24 +1,26 @@
 /**
  * Get all dealerships
  */
-
+const http = require('http');
 const Cloudant = require('@cloudant/cloudant');
+const cred = require('./../../../functions/.creds-sample.json')
 
-function main(params) {
-
+const requestListener = async function (req, res) {
+    console.log(cred.COUCH_URL)
+    console.log(cred.IAM_API_KEY)
     const cloudant = Cloudant({
-        url: params.COUCH_URL,
-        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+        url: cred.COUCH_URL,
+        plugins: { iamauth: { iamApiKey: cred.IAM_API_KEY } }
     });
-
-    let dbList = getDbs(cloudant);
-    return { dbs: dbList };
+    var db = cloudant.db.use('dealerships');
+    const doclist = await db.list({include_docs: true})
+    doclist.rows.forEach((doc) => {
+    console.log(doc["doc"]);
+    });
+    res.end(doclist.toString());
 }
 
-function getDbs(cloudant) {
-    cloudant.db.list().then((body) => {
-        body.forEach((db) => {
-            dbList.push(db);
-        });
-    }).catch((err) => { console.log(err); });
-}
+const port = 8080;
+const server = http.createServer(requestListener);
+console.log('server listening on port: ' + port);
+server.listen(port);
