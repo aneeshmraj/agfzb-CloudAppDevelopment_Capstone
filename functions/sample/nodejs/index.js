cd78d02451e2c18ec194edff51709bf9379c8efe
aneeshmraj <aneeshmraj@yahoo.com>
2021-09-15 14:13:20 +0000

    update endpoints

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -3,23 +3,59 @@
  */
 const http = require('http');
 const Cloudant = require('@cloudant/cloudant');
+const url = require('url');
 const cred = require('./../../../functions/.creds-sample.json')
 
 const requestListener = async function (req, res) {
-    console.log(cred.COUCH_URL)
-    console.log(cred.IAM_API_KEY)
+
     const cloudant = Cloudant({
         url: cred.COUCH_URL,
         plugins: { iamauth: { iamApiKey: cred.IAM_API_KEY } }
     });
-    var db = cloudant.db.use('dealerships');
-    const doclist = await db.list({include_docs: true})
-    doclist.rows.forEach((doc) => {
-    console.log(doc["doc"]);
-    });
-    res.end(doclist.toString());
+    console.log(req.url);
+    if (req.url === '/api/dealership'){
+        getAllDealerships(req,res,cloudant);
+    }
+    else if (req.url.startsWith('/api/dealership?state=')){
+        getAllDealershipsByState(req,res,cloudant);;
+    }
+    else{
+        res.end();
+    }
 }
 
+async function getAllDealerships(req, res, cloudant){
+    var db = cloudant.db.use('dealerships');
+    let dealerships = [];
+    const doclist = await db.list({include_docs: true})
+    doclist.rows.forEach((doc) => {
+    let dealer = doc["doc"];
+    delete dealer['_id'];
+    delete dealer['_rev'];    
+    dealerships.push(dealer);
+    });
+    res.end(JSON.stringify(dealerships));
+}
+
+async function getAllDealershipsByState(req,res, cloudant){
+    var db = cloudant.db.use('dealerships');
+    const queryObject = url.parse(req.url,true).query;
+    var query = {
+        "selector":{            
+            "st": queryObject['state']       
+        }
+    }    
+    let dealerships = [];
+    const doclist = await db.find(query);
+    doclist.docs.forEach(doc => {
+    let dealer = doc;
+    delete dealer['_id'];
+    delete dealer['_rev'];    
+    dealerships.push(dealer);
+    });
+    res.end(JSON.stringify(dealerships));
+    
+}
 const port = 8080;
 const server = http.createServer(requestListener);
 console.log('server listening on port: ' + port);
