d2c0ccedefff2c2f5fb98d90ad2eb83e80b450dc
aneeshmraj <aneeshmraj@yahoo.com>
2021-09-23 20:07:54 +0000

    updated

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -7,26 +7,90 @@
 # @return The output of this action, which must be a JSON object.
 #
 #
-from cloudant.client import Cloudant
-from cloudant.error import CloudantException
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_cloud_sdk_core import ApiException
+from requests import ConnectionError, ReadTimeout, RequestException, ValueError
 import requests
 
 
-def main(dict):
-    databaseName = "dealerships"
+def getAllReviewsForDealership(dict,dealerId):   
+
+    authenticator = IAMAuthenticator(dict["COUCH_URL"])
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url(dict["IAM_API_KEY"])
 
     try:
-        client = Cloudant.iam(
-            account_name=dict["COUCH_USERNAME"],
-            api_key=dict["IAM_API_KEY"],
-            connect=True,
-        )
-        print("Databases: {0}".format(client.all_dbs()))
-    except CloudantException as ce:
-        print("unable to connect")
-        return {"error": ce}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
+        # Invoke a Cloudant method request
+        response = service.post_search(
+                                        db='reviews',
+                                        query='dealership:'+dealerId
+                                        ).get_result()
 
-    return {"dbs": client.all_dbs()}
+        return response
+    
+    except ApiException as ae:
+        print("Method failed")
+        print(" - status code: " + str(ae.code))
+        print(" - error message: " + ae.message)
+    if ("reason" in ae.http_response.json()):
+        print(" - reason: " + ae.http_response.json()["reason"])
+    except ConnectionError as cerr:
+        print("Connection error occurred:")
+        print(cerr)
+    except ReadTimeout as rt:
+        # The server did not send any data in the allotted amount of time.
+        print("Read timed out:")
+        print(rt)
+    except RequestException as re:
+        # Handle other request failures
+        print("Request Exception:")
+        print(re)
+    except ValueError as ve:
+        print("Invalid argument value:\n" + ve.message)
+
+
+def postReviewForDealership(dict,review):   
+
+    authenticator = IAMAuthenticator(dict["COUCH_URL"])
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url(dict["IAM_API_KEY"])
+    review = review["review"]
+    add_review = Document(
+                    id=review["id"],
+                    name=review["name"],
+                    dealership=review["dealership"],
+                    review=review["review"],
+                    purchase=review["purchase"],
+                    another=review["another"],
+                    purchase_date=review["purchase_date"],
+                    car_make=review["car_make"],
+                    car_model=review["car_model"],
+                    car_year=review["car_year"])
+
+    try:        
+
+        response = service.post_document(db='reviews',
+                                         document=add_review).get_result()
+
+        return response
+    
+    except ApiException as ae:
+        print("Method failed")
+        print(" - status code: " + str(ae.code))
+        print(" - error message: " + ae.message)
+    if ("reason" in ae.http_response.json()):
+        print(" - reason: " + ae.http_response.json()["reason"])
+    except ConnectionError as cerr:
+        print("Connection error occurred:")
+        print(cerr)
+    except ReadTimeout as rt:
+        # The server did not send any data in the allotted amount of time.
+        print("Read timed out:")
+        print(rt)
+    except RequestException as re:
+        # Handle other request failures
+        print("Request Exception:")
+        print(re)
+    except ValueError as ve:
+        print("Invalid argument value:\n" + ve.message)
