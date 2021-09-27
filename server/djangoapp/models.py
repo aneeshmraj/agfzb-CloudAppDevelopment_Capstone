8804b38d9e5aefeef7cbe7c222d9dc03bea72ff1
aneeshmraj <aneeshmraj@yahoo.com>
2021-09-27 20:33:04 +0000

    updated models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,9 +9,79 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-class CarDealer:
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='make')
+    
+    description = models.CharField(max_length=1000)
+    
+    def __init__(self, name, description):
+        # name
+        self.name = name
+        # description
+        self.description = description
+        
+    def __str__(self):
+        return "Car Name: " + self.name + "," + \
+               "Description: " + self.description
 
-    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+
+
+
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
+# - Name
+# - Dealer id, used to refer a dealer created in cloudant database
+# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
+# - Year (DateField)
+# - Any other fields you would like to include in car model
+# - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    CAR_TYPE = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+    ]
+    name = models.CharField(null=False, max_length=30, default='model')
+    year = models.DateField(null=True)
+    dealer_id = models.IntegerField(default=0)
+    car_type = models.CharField(max_length=5, choices=CAR_TYPE, default=SEDAN)
+    make =  models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    
+    def __init__(self, name, dealer_id, car_type, year):
+        # name
+        self.name = name
+        # description
+        self.dealer_id = dealer_id
+        # type
+        self.car_type = car_type
+        #year
+        self.year = year
+
+        
+    def __str__(self):
+        return "Car model: " + self.name + "," + \
+               "type:" + self.car_type + ","+ \
+               "year" + self.year + "," + \
+               "dealer" + self.dealer 
+
+
+
+# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer(models.Model):
+    full_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=100)
+    lat = models.CharField(max_length=100)
+    long = models.CharField(max_length=100)
+    short_name = models.CharField(null=False, max_length=30, default='short name')
+    st = models.CharField(max_length=10)
+    zip = models.CharField(max_length=20)
+    dealer_id = models.IntegerField(default=0)
+
+    def __init__(self, address, city, full_name, dealer_id, lat, long, short_name, st, zip):
         # Dealer address
         self.address = address
         # Dealer city
@@ -19,7 +89,7 @@ class CarDealer:
         # Dealer Full Name
         self.full_name = full_name
         # Dealer id
-        self.id = id
+        self.dealer_id = dealer_id
         # Location lat
         self.lat = lat
         # Location long
@@ -33,18 +103,6 @@ class CarDealer:
 
     def __str__(self):
         return "Dealer name: " + self.full_name
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
