35a6ada9cb5cfc4cbdb3541580a25fada517937b
aneeshmraj <aneeshmraj@yahoo.com>
2021-10-07 12:43:11 +0000

    updated

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
-    path('dealer/addreview/<int:dealer_id>/', views.add_review, name='add_dealer_details')
+    path('dealer/addreview/<int:dealer_id>/', views.add_review, name='add_review_details')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -49,7 +49,7 @@ def login_request(request):
         if user is not None:
             # If user is valid, call login method to login current user
             login(request, user)
-            return redirect('djangoapp:get_dealerships')
+            return redirect('djangoapp:index')
         else:
             # If not, return to login page again
             return render(request, 'djangoapp/index.html', context)
@@ -63,7 +63,7 @@ def logout_request(request):
     # Logout user in the request
     logout(request)
     # Redirect user back to course list view
-    return redirect('djangoapp:get_dealerships')
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 def registration_request(request):
@@ -92,37 +92,38 @@ def registration_request(request):
                                             password=password)
             login(request, user)
             # redirect to course list page
-            return redirect("djangoapp:get_dealerships")
+            return redirect("djangoapp:index")
         else:
             return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
+        context = {}
         url = "https://us-south.functions.appdomain.cloud/api/v1/web/aneeshmraj%40yahoo.com_djangoserver-space/dealer/dealer-get.json"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url,
          COUCH_URL=COUCH_URL,
          IAM_API_KEY=IAM_API_KEY
          )
-        # Concat all dealer's short name
-        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
-        # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        context["dealership_list"] = dealerships
+        # Return a list of dealers as context
+        return render(request, 'djangoapp/index.html', context)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 def get_dealer_details(request, dealer_id):
     if request.method == "GET":
+        context={}
         url = "https://us-south.functions.appdomain.cloud/api/v1/web/aneeshmraj%40yahoo.com_djangoserver-space/reviews/review-get.json"
         # Get dealers from the URL
-        dealer_details = get_dealer_reviews_from_cf(url,
+        review_details = get_dealer_reviews_from_cf(url,
          COUCH_URL=COUCH_URL,
          IAM_API_KEY=IAM_API_KEY,
          dealerId = dealer_id
          )
-        # Concat all reviews
-        reviews = [detail.review for detail in dealer_details]
-        return HttpResponse(reviews)
+        context["reviews_list"] = review_details
+        # Concat all reviews        
+        return render(request,'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 def add_review(request, dealer_id):
